2da5f72ca5e9b5b21b80a80af4aff4db90de5af8
TamasPeti <gr1zh1@inf.elte.hu>
2023-04-02 12:27:45 +0200

    [CIT-94] > Industry LVL2 shape implemented

---
+++ City Builder/Engine/Shapes/Complex Shapes/Industry/IndustryBuilding2.cpp
@@ -0,0 +1,81 @@
+#include "IndustryBuilding2.h"
+
+IndustryBuilding2::IndustryBuilding2()
+{
+	shape_transform.push_back(glm::translate(glm::vec3(0.f, 0.3f, 0.2f)) * glm::scale(glm::vec3(1)));
+}
+
+void IndustryBuilding2::CreateBuffers()
+{
+
+	std::vector<Vertex> vertices;
+	std::vector<GLuint> indices;
+
+	CubeLayout cube_layout;
+
+	cube_layout.GeometryDetails.BOTTOM_BACK_LEFT = glm::vec3(-0.5, -0.3, -0.5);
+	cube_layout.GeometryDetails.BOTTOM_BACK_RIGHT = glm::vec3(0.5, -0.3, -0.5);
+	cube_layout.GeometryDetails.BOTTOM_FRONT_LEFT = glm::vec3(-0.5, -0.3, 0);
+	cube_layout.GeometryDetails.BOTTOM_FRONT_RIGHT = glm::vec3(0.5, -0.3, 0);
+
+	cube_layout.GeometryDetails.TOP_BACK_LEFT = glm::vec3(-0.5, 0.3, -0.5);
+	cube_layout.GeometryDetails.TOP_BACK_RIGHT = glm::vec3(0.5, 0.3, -0.5);
+	cube_layout.GeometryDetails.TOP_FRONT_LEFT = glm::vec3(-0.5, 0.3, 0);
+	cube_layout.GeometryDetails.TOP_FRONT_RIGHT = glm::vec3(0.5, 0.3, 0);
+
+	cube_layout.TextureDetails.BACK_ID = 0.f;
+	cube_layout.TextureDetails.FRONT_ID = 0.f;
+	cube_layout.TextureDetails.RIGHT_ID = 0.f;
+	cube_layout.TextureDetails.LEFT_ID = 0.f;
+	cube_layout.TextureDetails.TOP_ID = 1.f;
+	cube_layout.TextureDetails.BOTTOM_ID = 1.f;
+
+	Shape::ConcatenateIndices(indices, Cube::GenerateIndices(cube_layout, (GLuint)vertices.size()));
+	Shape::ConcatenateVertices(vertices, Cube::GenerateVertices(cube_layout));
+
+	cube_layout.GeometryDetails.BOTTOM_BACK_LEFT = glm::vec3(-0.5, -0.3, 0);
+	cube_layout.GeometryDetails.BOTTOM_BACK_RIGHT = glm::vec3(0.5, -0.3, 0);
+	cube_layout.GeometryDetails.BOTTOM_FRONT_LEFT = glm::vec3(-0.5, -0.3, 0.5);
+	cube_layout.GeometryDetails.BOTTOM_FRONT_RIGHT = glm::vec3(0.5, -0.3, 0.5);
+
+	cube_layout.GeometryDetails.TOP_BACK_LEFT = glm::vec3(-0.5, 0, 0);
+	cube_layout.GeometryDetails.TOP_BACK_RIGHT = glm::vec3(0.5, 0, 0);
+	cube_layout.GeometryDetails.TOP_FRONT_LEFT = glm::vec3(-0.5, 0, 0.5);
+	cube_layout.GeometryDetails.TOP_FRONT_RIGHT = glm::vec3(0.5, 0, 0.5);
+
+	Shape::ConcatenateIndices(indices, Cube::GenerateIndices(cube_layout, (GLuint)vertices.size()));
+	Shape::ConcatenateVertices(vertices, Cube::GenerateVertices(cube_layout));
+
+	CylinderLayout cylinder_layout;
+	cylinder_layout.GeometryDetails.COUNT = 10;
+	cylinder_layout.GeometryDetails.BOTTOM_ORIGO = glm::vec3(-0.3, -0.3, -0.7);
+	cylinder_layout.GeometryDetails.BOTTOM_RADIUS = 0.15f;
+	cylinder_layout.GeometryDetails.TOP_ORIGO = glm::vec3(-0.3, 1, -0.7);
+	cylinder_layout.GeometryDetails.TOP_RADIUS = 0.1f;
+	cylinder_layout.TextureDetails.WALL_ID = 2.f;
+	cylinder_layout.TextureDetails.CIRCLE_ID = 3.f;
+
+	Shape::ConcatenateIndices(indices, Cylinder::GenerateIndices(cylinder_layout, (GLuint)vertices.size()));
+	Shape::ConcatenateVertices(vertices, Cylinder::GenerateVertices(cylinder_layout));
+
+	cylinder_layout.GeometryDetails.COUNT = 10;
+	cylinder_layout.GeometryDetails.BOTTOM_ORIGO = glm::vec3(0, -0.3, -0.7);
+	cylinder_layout.GeometryDetails.BOTTOM_RADIUS = 0.15f;
+	cylinder_layout.GeometryDetails.TOP_ORIGO = glm::vec3(0, 1, -0.7);
+	cylinder_layout.GeometryDetails.TOP_RADIUS = 0.1f;
+
+	Shape::ConcatenateIndices(indices, Cylinder::GenerateIndices(cylinder_layout, (GLuint)vertices.size()));
+	Shape::ConcatenateVertices(vertices, Cylinder::GenerateVertices(cylinder_layout));
+
+	cylinder_layout.GeometryDetails.COUNT = 10;
+	cylinder_layout.GeometryDetails.BOTTOM_ORIGO = glm::vec3(0.3, -0.3, -0.7);
+	cylinder_layout.GeometryDetails.BOTTOM_RADIUS = 0.15f;
+	cylinder_layout.GeometryDetails.TOP_ORIGO = glm::vec3(0.3, 1, -0.7);
+	cylinder_layout.GeometryDetails.TOP_RADIUS = 0.1f;
+
+	Shape::ConcatenateIndices(indices, Cylinder::GenerateIndices(cylinder_layout, (GLuint)vertices.size()));
+	Shape::ConcatenateVertices(vertices, Cylinder::GenerateVertices(cylinder_layout));
+
+	AttachToGPU(vertices, indices);
+
+}
