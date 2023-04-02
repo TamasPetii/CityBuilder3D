f77ba0884a8da5196b49e202a0525242da4b1ede
TamasPeti <gr1zh1@inf.elte.hu>
2023-04-02 12:07:33 +0200

    [CIT-125] >Added application class

---
+++ City Builder/Application/Application.cpp
@@ -0,0 +1,274 @@
+#include "Application.h"
+
+Application::Application(GLFWwindow* window, int WINDOW_WIDTH, int WINDOW_HEIGHT)
+	:
+	m_Window(window),
+	m_WindowWidth(WINDOW_WIDTH),
+	m_WindowHeight(WINDOW_HEIGHT)
+{
+
+	m_Camera = new Camera(WINDOW_WIDTH, WINDOW_HEIGHT);
+	m_GameTable = new GameTable(50);
+	m_Renderer = new Renderer(m_Camera);
+	m_MyGui = new MyGui(m_Camera);
+
+	m_Camera->Set_Eye(glm::vec3(m_GameTable->Get_TableSize(), 5, m_GameTable->Get_TableSize() + 5));
+	m_Camera->Set_At(glm::vec3(m_GameTable->Get_TableSize(), 0, m_GameTable->Get_TableSize()));
+}
+
+Application::~Application()
+{
+	delete m_Camera;
+	delete m_Renderer;
+	delete m_MyGui;
+	delete m_GameTable;
+}
+
+void Application::Update()
+{
+	m_Camera->Update();
+
+	//------------------------------
+
+	if (m_MyGui->hit)
+	{
+		std::cout << "---------FROM APPLICATION--------" << std::endl;
+		ConvertMouseInputTo3D(m_MyGui->mouse_x, m_MyGui->mouse_y, (int)m_MyGui->content_size.x, (int)m_MyGui->content_size.y);
+		ConvertMouseInputTo3D(m_MyGui->mouse_x, m_MyGui->mouse_y, m_Renderer->Get_FrameBuffer()->Get_FrameWidth(), m_Renderer->Get_FrameBuffer()->Get_FrameHeight());
+		std::cout << "---------------------------------" << std::endl;
+		m_MyGui->hit = false;
+	}
+
+
+	//------------------------------
+
+	static double lastTime = glfwGetTime();
+
+	double m_CurrentTime = glfwGetTime();
+	m_FrameCounter++;
+
+	if (m_CurrentTime - lastTime >= 1.0)
+	{
+		std::cout << m_FrameCounter << std::endl;
+
+		m_FrameCounter = 0;
+		lastTime += 1.0;
+	}
+}
+
+void Application::RenderUI()
+{
+	m_MyGui->Pre_Render();
+
+	m_MyGui->DockSpace_Render();
+	m_MyGui->Demo_Render();
+	m_MyGui->Window1_Render();
+	m_MyGui->Window2_Render();
+	m_MyGui->Window3_Render();
+	m_MyGui->Window4_Render();
+	m_MyGui->Window5_Render();
+	m_MyGui->ViewPort_Render(m_Renderer->Get_FrameBuffer());
+
+	m_MyGui->Post_Render();
+}
+
+void Application::Render()
+{
+	if (changed)
+	{
+		transforms_CUBE.clear();
+		transforms_CONE.clear();
+		transforms_CYLINDER.clear();
+		transforms_PYRAMID.clear();
+		transforms_SPHERE.clear();
+
+		transforms_EMPTY.clear();
+		transforms_ROAD.clear();
+		transforms_FOREST.clear();
+
+		transforms_RESIDENCE1.clear();
+		transforms_RESIDENCE2.clear();
+		transforms_RESIDENCE3.clear();
+
+		transforms_INDUSTRY1.clear();
+		transforms_INDUSTRY2.clear();
+		transforms_INDUSTRY3.clear();
+
+		transforms_SERVICE1.clear();
+		transforms_SERVICE2.clear();
+		transforms_SERVICE3.clear();
+
+		transforms_FIRESTATION.clear();
+		transforms_POLICESTATION.clear();
+		transforms_STADION.clear();
+
+		transforms_POWERSTATION.clear();
+		transforms_POWERWIRE.clear();
+
+		transforms_SCHOOL1.clear();
+		transforms_SCHOOL2.clear();
+
+		transforms_CHARACTER.clear();
+
+		for (int i = 0; i < m_GameTable->Get_TableSize(); i++)
+		{
+			for (int j = 0; j < m_GameTable->Get_TableSize(); j++)
+			{
+				glm::mat4 transform = glm::translate(glm::vec3(2 * j + 1, 0, 2 * i + 1));
+
+				switch (m_GameTable->Get_TableValue(i, j))
+				{
+				case 0: transforms_EMPTY.push_back(transform);  break;
+				case 1: transforms_ROAD.push_back(transform);   break;
+				case 2: transforms_FOREST.push_back(transform); break;
+				case 3: transforms_RESIDENCE1.push_back(transform); break;
+				case 4: transforms_RESIDENCE2.push_back(transform); break;
+				case 5: transforms_RESIDENCE3.push_back(transform); break;
+				case 6: transforms_SERVICE1.push_back(transform); break;
+				case 7: transforms_SERVICE2.push_back(transform); break;
+				case 8: transforms_SERVICE3.push_back(transform); break;
+				case 9: transforms_FIRESTATION.push_back(transform); break;
+				case 10: transforms_POLICESTATION.push_back(transform); break;
+				case 11: transforms_STADION.push_back(transform); break;
+				case 12: transforms_SCHOOL1.push_back(transform); break;
+				case 13: transforms_SCHOOL2.push_back(transform); break;
+				case 14: transforms_POWERSTATION.push_back(transform); break;
+				case 15: transforms_POWERWIRE.push_back(transform); break;
+				case 16: transforms_INDUSTRY1.push_back(transform); break;
+				case 17: transforms_INDUSTRY2.push_back(transform); break;
+				case 18: transforms_INDUSTRY3.push_back(transform); break;
+				case 19: transforms_CHARACTER.push_back(transform); break;
+				}
+			}
+		}
+		changed = false;
+	}
+
+	m_Renderer->Render_PreRender();
+	m_Renderer->RenderInstanced_Character(transforms_CHARACTER);
+	m_Renderer->RenderInstanced_Empty(transforms_EMPTY);
+	m_Renderer->RenderInstanced_Road(transforms_ROAD);
+	m_Renderer->RenderInstanced_Forest(transforms_FOREST);
+	m_Renderer->RenderInstanced_Residence1(transforms_RESIDENCE1);
+	m_Renderer->RenderInstanced_Residence2(transforms_RESIDENCE2);
+	m_Renderer->RenderInstanced_Residence3(transforms_RESIDENCE3);
+	m_Renderer->RenderInstanced_Industry1(transforms_INDUSTRY1);
+	m_Renderer->RenderInstanced_Industry2(transforms_INDUSTRY2);
+	m_Renderer->RenderInstanced_Industry3(transforms_INDUSTRY3);
+	m_Renderer->RenderInstanced_Service1(transforms_SERVICE1);
+	m_Renderer->RenderInstanced_Service2(transforms_SERVICE2);
+	m_Renderer->RenderInstanced_Service3(transforms_SERVICE3);
+	m_Renderer->RenderInstanced_FireStation(transforms_FIRESTATION);
+	m_Renderer->RenderInstanced_PoliceStation(transforms_POLICESTATION);
+	m_Renderer->RenderInstanced_Stadion(transforms_STADION);
+	m_Renderer->RenderInstanced_PowerStation(transforms_POWERSTATION);
+	m_Renderer->RenderInstanced_PowerWire(transforms_POWERWIRE);
+	m_Renderer->RenderInstanced_School1(transforms_SCHOOL1);
+	m_Renderer->RenderInstanced_School2(transforms_SCHOOL2);
+	m_Renderer->Render_Axis();
+	m_Renderer->Render_Ray(RayOrigin, RayEnd);
+	m_Renderer->Render_PostRender();
+}
+
+
+//---------------------------------------------------------|Events|---------------------------------------------------------//
+//---------------------------------------------------------|Events|---------------------------------------------------------//
+//---------------------------------------------------------|Events|---------------------------------------------------------//
+
+void Application::Window_ResizedEvent(int width, int height)
+{
+	m_WindowWidth = width;
+	m_WindowHeight = height;
+
+	m_Renderer->Set_WindowSize(width, height);
+};
+
+void Application::FrameBuffer_ResizedEvent(int width, int height)
+{
+	m_Camera->Set_ProjMatrix(width, height);
+};
+
+void Application::Keyboard_ButtonEvent(int key, int scancode, int action, int mods)
+{
+	if (ImGui::GetIO().WantCaptureKeyboard) return;
+
+	m_Camera->Keyboard_ButtonEvent(key, scancode, action, mods);
+};
+
+void Application::Mouse_MoveEvent(double xpos, double ypos)
+{
+	if (ImGui::GetIO().WantCaptureMouse) return;
+
+	m_Camera->Mouse_MoveEvent((float)xpos, (float)ypos);
+};
+
+void Application::Mouse_ClickEvent(int button, int action, int mods)
+{
+	if (ImGui::GetIO().WantCaptureMouse) return;
+
+	m_Camera->Mouse_ClickEvent(button, action, mods);
+
+	if (action == GLFW_PRESS && button == GLFW_MOUSE_BUTTON_RIGHT)
+	{
+		double xpos, ypos;
+		glfwGetCursorPos(m_Window, &xpos, &ypos);
+	}
+};
+
+void Application::Mouse_WeelEvent(double xoffset, double yoffset)
+{
+	if (ImGui::GetIO().WantCaptureMouse) return;
+
+	m_Camera->Mouse_WheelEvent((float)xoffset, (float)yoffset);
+};
+
+void Application::ConvertMouseInputTo3D(int xpos, int ypos, int width, int height)
+{
+	//Turning [x,y] coordinates to Normalized Device Coordinate system [-1,1]
+	float xnorm = (xpos / (float)width - 0.5f) * 2.f;
+	float ynorm = (ypos / (float)height - 0.5f) * -2.f;
+
+	//[z = -1.] camera starting point in NDC
+	//[z = 1.f] the furthest point in NDC
+	glm::vec4 startRayNorm(xnorm, ynorm, -1.f, 1.f);
+	glm::vec4 endRayNorm(xnorm, ynorm, 1.f, 1.f);
+
+	//We need to go backwards in coordinate systems.
+	//NDC -> PROJ -> VIEW -> WORLD
+	glm::mat4 matrix = glm::inverse(m_Camera->Get_ViewProjMatrix());
+	glm::vec4 startRayWorld = matrix * startRayNorm; startRayWorld /= startRayWorld.w;
+	glm::vec4 endRayWorld = matrix * endRayNorm; endRayWorld /= endRayWorld.w;
+
+	RayOrigin = startRayWorld;
+	RayEnd = endRayWorld;
+	RayDirection = glm::normalize(endRayWorld - startRayWorld);
+
+	/*
+	The whole map is at [y = 0.5f] so we need to find that point in the ray which height = 0.5f
+	We use the parametric equation to determine the x,y values
+	*/
+
+	float t = (0.f - startRayWorld.y) / RayDirection.y;
+	RayHit = glm::vec3(RayOrigin.x + t * RayDirection.x, 0.0, RayOrigin.z + t * RayDirection.z);
+
+	/*
+	Checking possible ray and field hit.
+	If possible then we determine the [x,y] values which represent the indices of the GameField
+	*/
+
+	float rayx = RayHit.x;
+	float rayz = RayHit.z;
+
+	bool l_1 = rayx > 0.f && rayx < m_GameTable->Get_TableSize() * 2 && t >= 0;
+	bool l_2 = rayz > 0.f && rayz < m_GameTable->Get_TableSize() * 2 && t >= 0;
+
+	if (l_1 && l_2)
+	{
+
+		rayx /= 2.f;
+		rayz /= 2.f;
+
+		//m_GameTable->Set_TableValue(int(rayz), int(rayx), m_Settings->BuildingID);
+		changed = true;
+	}
+}
---
+++ City Builder/Application/Application.h
@@ -0,0 +1,81 @@
+#pragma once
+//OpenGL Headers
+#include <GLEW/glew.h>
+#include <GLFW/glfw3.h>
+#include <GLM/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+#include <GLM/gtx/transform2.hpp>
+
+//Project Headers
+#include "MyGui/MyGui.h"
+#include "Abstractions/Camera.h"
+#include "Renderer/Renderer.h"
+#include "GameTable.h"
+
+//Std Headers
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Application
+{
+public:
+	Application(GLFWwindow* window, int WINDOW_WIDTH, int WINDOW_HEIGHT);
+	~Application();
+
+	void Update();
+	void Render();
+	void RenderUI();
+
+	void Window_ResizedEvent(int width, int height);
+	void FrameBuffer_ResizedEvent(int width, int height);
+	void Keyboard_ButtonEvent(int key, int scancode, int action, int mods);
+	void Mouse_MoveEvent(double xpos, double ypos);
+	void Mouse_ClickEvent(int button, int action, int mods);
+	void Mouse_WeelEvent(double xoffset, double yoffset);
+
+	void ConvertMouseInputTo3D(int xpos, int ypos, int width, int height);
+private:
+	GLFWwindow* m_Window = nullptr;
+	Camera* m_Camera = nullptr;
+	Renderer* m_Renderer = nullptr;
+	MyGui* m_MyGui = nullptr;
+	GameTable* m_GameTable = nullptr;
+private:
+	std::vector<glm::mat4> transforms_CUBE;
+	std::vector<glm::mat4> transforms_CONE;
+	std::vector<glm::mat4> transforms_CYLINDER;
+	std::vector<glm::mat4> transforms_PYRAMID;
+	std::vector<glm::mat4> transforms_SPHERE;
+	std::vector<glm::mat4> transforms_RESIDENCE1;
+	std::vector<glm::mat4> transforms_RESIDENCE2;
+	std::vector<glm::mat4> transforms_RESIDENCE3;
+	std::vector<glm::mat4> transforms_INDUSTRY1;
+	std::vector<glm::mat4> transforms_INDUSTRY2;
+	std::vector<glm::mat4> transforms_INDUSTRY3;
+	std::vector<glm::mat4> transforms_SERVICE1;
+	std::vector<glm::mat4> transforms_SERVICE2;
+	std::vector<glm::mat4> transforms_SERVICE3;
+	std::vector<glm::mat4> transforms_FIRESTATION;
+	std::vector<glm::mat4> transforms_POLICESTATION;
+	std::vector<glm::mat4> transforms_POWERSTATION;
+	std::vector<glm::mat4> transforms_POWERWIRE;
+	std::vector<glm::mat4> transforms_STADION;
+	std::vector<glm::mat4> transforms_SCHOOL1;
+	std::vector<glm::mat4> transforms_SCHOOL2;
+	std::vector<glm::mat4> transforms_CHARACTER;
+	std::vector<glm::mat4> transforms_EMPTY;
+	std::vector<glm::mat4> transforms_ROAD;
+	std::vector<glm::mat4> transforms_FOREST;
+private:
+	glm::vec3 RayOrigin = glm::vec3(0);
+	glm::vec3 RayEnd = glm::vec3(0);
+	glm::vec3 RayDirection = glm::vec3(0);
+	glm::vec3 RayHit = glm::vec3(0);
+
+	bool changed = true;
+	int m_FrameCounter = 0;
+	int m_WindowWidth = 0;
+	int m_WindowHeight = 0;
+};
+
