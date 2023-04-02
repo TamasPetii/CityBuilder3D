e88bf9685fab3c9008b833a97e1bba3e1a5894aa
TamasPeti <gr1zh1@inf.elte.hu>
2023-04-02 12:43:03 +0200

    [CIT-119] > Added a basic UI

---
+++ City Builder/ImGui/MyGui/MyGui.cpp
@@ -0,0 +1,352 @@
+﻿#include "MyGui.h"
+
+
+void MyGui::Init(GLFWwindow* window)
+{
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
+    Custom_Style();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+}
+
+void MyGui::Custom_Style()
+{
+    ImGui::StyleColorsDark();
+    ImGuiStyle& style = ImGui::GetStyle();
+
+    style.Colors[ImGuiCol_Text] = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+    style.Colors[ImGuiCol_TextDisabled] = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
+    style.Colors[ImGuiCol_WindowBg] = ImVec4(0.13f, 0.14f, 0.15f, 1.00f);
+    style.Colors[ImGuiCol_ChildBg] = ImVec4(0.13f, 0.14f, 0.15f, 1.00f);
+    style.Colors[ImGuiCol_PopupBg] = ImVec4(0.13f, 0.14f, 0.15f, 1.00f);
+    style.Colors[ImGuiCol_Border] = ImVec4(0.43f, 0.43f, 0.50f, 0.50f);
+    style.Colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+    style.Colors[ImGuiCol_FrameBg] = ImVec4(0.25f, 0.25f, 0.25f, 1.00f);
+    style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.38f, 0.38f, 0.38f, 1.00f);
+    style.Colors[ImGuiCol_FrameBgActive] = ImVec4(0.67f, 0.67f, 0.67f, 0.39f);
+    style.Colors[ImGuiCol_TitleBg] = ImVec4(0.08f, 0.08f, 0.09f, 1.00f);
+    style.Colors[ImGuiCol_TitleBgActive] = ImVec4(0.08f, 0.08f, 0.09f, 1.00f);
+    style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.00f, 0.00f, 0.00f, 0.51f);
+    style.Colors[ImGuiCol_MenuBarBg] = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
+    style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.02f, 0.02f, 0.02f, 0.53f);
+    style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.31f, 0.31f, 0.31f, 1.00f);
+    style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.41f, 0.41f, 0.41f, 1.00f);
+    style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.51f, 0.51f, 0.51f, 1.00f);
+    style.Colors[ImGuiCol_CheckMark] = ImVec4(0.11f, 0.64f, 0.92f, 1.00f);
+    style.Colors[ImGuiCol_SliderGrab] = ImVec4(0.11f, 0.64f, 0.92f, 1.00f);
+    style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.08f, 0.50f, 0.72f, 1.00f);
+    style.Colors[ImGuiCol_Button] = ImVec4(0.25f, 0.25f, 0.25f, 1.00f);
+    style.Colors[ImGuiCol_ButtonHovered] = ImVec4(0.38f, 0.38f, 0.38f, 1.00f);
+    style.Colors[ImGuiCol_ButtonActive] = ImVec4(0.67f, 0.67f, 0.67f, 0.39f);
+    style.Colors[ImGuiCol_Header] = ImVec4(0.22f, 0.22f, 0.22f, 1.00f);
+    style.Colors[ImGuiCol_HeaderHovered] = ImVec4(0.25f, 0.25f, 0.25f, 1.00f);
+    style.Colors[ImGuiCol_HeaderActive] = ImVec4(0.67f, 0.67f, 0.67f, 0.39f);
+    style.Colors[ImGuiCol_Separator] = style.Colors[ImGuiCol_Border];
+    style.Colors[ImGuiCol_SeparatorHovered] = ImVec4(0.41f, 0.42f, 0.44f, 1.00f);
+    style.Colors[ImGuiCol_SeparatorActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.95f);
+    style.Colors[ImGuiCol_ResizeGrip] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+    style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.29f, 0.30f, 0.31f, 0.67f);
+    style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.95f);
+    style.Colors[ImGuiCol_Tab] = ImVec4(0.08f, 0.08f, 0.09f, 0.83f);
+    style.Colors[ImGuiCol_TabHovered] = ImVec4(0.33f, 0.34f, 0.36f, 0.83f);
+    style.Colors[ImGuiCol_TabActive] = ImVec4(0.23f, 0.23f, 0.24f, 1.00f);
+    style.Colors[ImGuiCol_TabUnfocused] = ImVec4(0.08f, 0.08f, 0.09f, 1.00f);
+    style.Colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.13f, 0.14f, 0.15f, 1.00f);
+    style.Colors[ImGuiCol_DockingPreview] = ImVec4(0.26f, 0.59f, 0.98f, 0.70f);
+    style.Colors[ImGuiCol_DockingEmptyBg] = ImVec4(0.20f, 0.20f, 0.20f, 1.00f);
+    style.Colors[ImGuiCol_PlotLines] = ImVec4(0.61f, 0.61f, 0.61f, 1.00f);
+    style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
+    style.Colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+    style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
+    style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.26f, 0.59f, 0.98f, 0.35f);
+    style.Colors[ImGuiCol_DragDropTarget] = ImVec4(0.11f, 0.64f, 0.92f, 1.00f);
+    style.Colors[ImGuiCol_NavHighlight] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+    style.Colors[ImGuiCol_NavWindowingHighlight] = ImVec4(1.00f, 1.00f, 1.00f, 0.70f);
+    style.Colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.20f);
+    style.Colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.35f);
+    style.GrabRounding = style.FrameRounding = 2.3f;
+
+    if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+        style.WindowMinSize = ImVec2(100, 100);
+    }
+}
+
+void MyGui::Clean()
+{
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+}
+
+//------------------------------------------------------------|Non-static|------------------------------------------------------------//
+
+MyGui::MyGui(Camera* camera) : m_Camera(camera) {}
+
+
+
+void MyGui::Pre_Render()
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+}
+
+void MyGui::Post_Render()
+{
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        GLFWwindow* backup_current_context = glfwGetCurrentContext();
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+        glfwMakeContextCurrent(backup_current_context);
+    }
+}
+
+void MyGui::DockSpace_Render()
+{
+    static bool opt_fullscreen = true;
+    static bool opt_padding = false;
+    static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+    if (opt_fullscreen)
+    {
+        const ImGuiViewport* viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->WorkPos);
+        ImGui::SetNextWindowSize(viewport->WorkSize);
+        ImGui::SetNextWindowViewport(viewport->ID);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+        window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+    }
+    else
+    {
+        dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+    }
+
+    if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+        window_flags |= ImGuiWindowFlags_NoBackground;
+
+    if (!opt_padding)
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+    ImGui::Begin("DockSpace Demo", nullptr, window_flags);
+    if (!opt_padding)
+        ImGui::PopStyleVar();
+
+    if (opt_fullscreen)
+        ImGui::PopStyleVar(2);
+
+    // Submit the DockSpace
+    ImGuiIO& io = ImGui::GetIO();
+    if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+    {
+        ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+    }
+
+    if (ImGui::BeginMenuBar())
+    {
+
+        if (ImGui::BeginMenu("File"))
+        {
+            if (ImGui::MenuItem("New Game"))
+            {
+                ImGui::OpenPopup("New Game Pop Up");
+                std::cout << "Clicked on New Game menuitem" << std::endl;
+            }
+            if (ImGui::MenuItem("Load Game"))
+            {
+                std::cout << "Clicked on Load Game menuitem" << std::endl;
+            }
+            if (ImGui::MenuItem("Save Game"))
+            {
+                std::cout << "Clicked on Save Game menuitem" << std::endl;
+            }
+            ImGui::EndMenu();
+        }
+
+        if (ImGui::BeginMenu("Options"))
+        {
+            // Disabling fullscreen would allow the window to be moved to the front of other windows,
+            // which we can't undo at the moment without finer window depth/z control.
+            ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen);
+            ImGui::MenuItem("Padding", NULL, &opt_padding);
+            ImGui::Separator();
+
+            if (ImGui::MenuItem("Flag: NoSplit", "", (dockspace_flags & ImGuiDockNodeFlags_NoSplit) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoSplit; }
+            if (ImGui::MenuItem("Flag: NoResize", "", (dockspace_flags & ImGuiDockNodeFlags_NoResize) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoResize; }
+            if (ImGui::MenuItem("Flag: NoDockingInCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingInCentralNode) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoDockingInCentralNode; }
+            if (ImGui::MenuItem("Flag: AutoHideTabBar", "", (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar; }
+            if (ImGui::MenuItem("Flag: PassthruCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0, opt_fullscreen)) { dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode; }
+            ImGui::Separator();
+            ImGui::EndMenu();
+        }
+        ImGui::EndMenuBar();
+    }
+
+    ImGui::End();
+}
+
+void MyGui::ViewPort_Render(FrameBuffer* fbo)
+{
+    ImGui::Begin("ViewPort", nullptr, ImGuiWindowFlags_NoCollapse);
+
+    if (ImGui::IsWindowFocused())
+    {
+        Camera_MouseClickEvent();
+        Camera_KeyboardKeyEvent();
+        Build_MouseClickEvent();
+    }
+
+    ImVec2 size = ImGui::GetContentRegionAvail();
+
+    ImGui::Image((void*)fbo->Get_TextureId(), size, ImVec2(0, 1), ImVec2(1, 0));
+
+    if (size.x != m_ViewPort_Width || size.y != m_ViewPort_Height)
+    {
+        fbo->Resize((int)size.x, (int)size.y);
+    }
+
+    m_ViewPort_Width = (int)size.x;
+    m_ViewPort_Height = (int)size.y;
+
+    ImGui::End();
+}
+
+void MyGui::Demo_Render()
+{
+    ImGui::ShowDemoWindow();
+}
+
+void MyGui::Window1_Render()
+{
+    ImGui::Begin("Beállítás");
+    ImGui::End();
+}
+
+void MyGui::Window2_Render()
+{
+    ImGui::Begin("Általános adatok");
+    ImGui::End();
+}
+
+void MyGui::Window3_Render()
+{
+    ImGui::Begin("Rombolás");
+    ImGui::End();
+}
+
+void MyGui::Window4_Render()
+{
+    ImGui::Begin("Építés");
+    ImGui::End();
+}
+
+void MyGui::Window5_Render()
+{
+    ImGui::Begin("Mező adatok");
+    ImGui::End();
+}
+
+//---------------------------------------------------------|EVENTS|---------------------------------------------------------//
+//---------------------------------------------------------|EVENTS|---------------------------------------------------------//
+//---------------------------------------------------------|EVENTS|---------------------------------------------------------//
+void MyGui::Build_MouseClickEvent()
+{
+    if (ImGui::IsMouseClicked(ImGuiMouseButton_Right))
+    {
+        int RelativWindowCursor_X = (int)(ImGui::GetMousePos().x - ImGui::GetWindowPos().x);
+        int RelativWindowCursor_Y = (int)(ImGui::GetMousePos().y - ImGui::GetWindowPos().y);
+
+        ImVec2 CurrentContent_MIN = ImGui::GetWindowContentRegionMin();
+        ImVec2 CurrentContent_MAX = ImGui::GetWindowContentRegionMax();
+
+        bool CursorInContentAreaMin_X = RelativWindowCursor_X >= CurrentContent_MIN.x && RelativWindowCursor_Y >= CurrentContent_MIN.y;
+        bool CursorInContentAreaMax_Y = RelativWindowCursor_X <= CurrentContent_MAX.x && RelativWindowCursor_Y <= CurrentContent_MAX.y;
+
+        if (CursorInContentAreaMin_X && CursorInContentAreaMax_Y)
+        {
+            hit = true;
+            mouse_x = RelativWindowCursor_X - (int)ImGui::GetWindowContentRegionMin().x;
+            mouse_y = RelativWindowCursor_Y - (int)ImGui::GetWindowContentRegionMin().y;
+            content_size.x = CurrentContent_MAX.x - CurrentContent_MIN.x;
+            content_size.y = CurrentContent_MAX.y - CurrentContent_MIN.y;
+            std::cout << "Hitting content area: " << "{x = " << RelativWindowCursor_X - ImGui::GetWindowContentRegionMin().x << "} | {y = } " << RelativWindowCursor_Y - ImGui::GetWindowContentRegionMin().y << "}" << std::endl;
+        }
+    }
+}
+
+void MyGui::Camera_MouseClickEvent()
+{
+    if (ImGui::IsMouseClicked(ImGuiMouseButton_Left))
+    {
+        m_Camera->Mouse_ClickEvent(GLFW_MOUSE_BUTTON_LEFT, GLFW_PRESS, 0);
+    }
+    if (ImGui::IsMouseReleased(ImGuiMouseButton_Left))
+    {
+        m_Camera->Mouse_ClickEvent(GLFW_MOUSE_BUTTON_LEFT, GLFW_RELEASE, 0);
+    }
+    if (ImGui::IsWindowHovered())
+    {
+        m_Camera->Mouse_MoveEvent(ImGui::GetIO().MousePos.x, ImGui::GetIO().MousePos.y);
+    }
+}
+
+void MyGui::Camera_KeyboardKeyEvent()
+{
+    //W
+    if (ImGui::IsKeyPressed(ImGuiKey_W))
+    {
+        m_Camera->Keyboard_ButtonEvent(GLFW_KEY_W, 0, GLFW_PRESS, 0);
+    }
+    if (ImGui::IsKeyReleased(ImGuiKey_W))
+    {
+        m_Camera->Keyboard_ButtonEvent(GLFW_KEY_W, 0, GLFW_RELEASE, 0);
+    }
+
+    //S
+    if (ImGui::IsKeyPressed(ImGuiKey_S))
+    {
+        m_Camera->Keyboard_ButtonEvent(GLFW_KEY_S, 0, GLFW_PRESS, 0);
+    }
+    if (ImGui::IsKeyReleased(ImGuiKey_S))
+    {
+        m_Camera->Keyboard_ButtonEvent(GLFW_KEY_S, 0, GLFW_RELEASE, 0);
+    }
+
+    //A
+    if (ImGui::IsKeyPressed(ImGuiKey_A))
+    {
+        m_Camera->Keyboard_ButtonEvent(GLFW_KEY_A, 0, GLFW_PRESS, 0);
+    }
+    if (ImGui::IsKeyReleased(ImGuiKey_A))
+    {
+        m_Camera->Keyboard_ButtonEvent(GLFW_KEY_A, 0, GLFW_RELEASE, 0);
+    }
+
+    //D
+    if (ImGui::IsKeyPressed(ImGuiKey_D))
+    {
+        m_Camera->Keyboard_ButtonEvent(GLFW_KEY_D, 0, GLFW_PRESS, 0);
+    }
+    if (ImGui::IsKeyReleased(ImGuiKey_D))
+    {
+        m_Camera->Keyboard_ButtonEvent(GLFW_KEY_D, 0, GLFW_RELEASE, 0);
+    }
+}
---
+++ City Builder/ImGui/MyGui/MyGui.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <GLEW/glew.h>
+#include <GLFW/glfw3.h>
+#include "Abstractions/Camera.h"
+#include "Abstractions/FrameBuffer.h"
+
+#include "../ImGui/imgui.h"
+#include "../ImGui/imgui_impl_glfw.h"
+#include "../ImGui/imgui_impl_opengl3.h"
+
+class MyGui
+{
+public:
+	MyGui(Camera* camera);
+	~MyGui() = default;
+
+	void Pre_Render();
+	void Post_Render();
+
+	void Demo_Render();
+	void Window1_Render();
+	void Window2_Render();
+	void Window3_Render();
+	void Window4_Render();
+	void Window5_Render();
+	void ViewPort_Render(FrameBuffer* fbo);
+	void DockSpace_Render();
+
+	static void Init(GLFWwindow* window);
+	static void Clean();
+
+	bool hit;
+	int mouse_x;
+	int mouse_y;
+	ImVec2 content_size;
+protected:
+	//Style
+	static void Custom_Style();
+	//Events
+	void Build_MouseClickEvent();
+	void Camera_MouseClickEvent();
+	void Camera_KeyboardKeyEvent();
+private:
+	Camera* m_Camera = nullptr;
+	int m_ViewPort_Width = 0;
+	int m_ViewPort_Height = 0;
+};
+
