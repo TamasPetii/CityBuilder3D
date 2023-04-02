00cf32909431ee928f6e60f37611bfcd3671593d
TamasPeti <gr1zh1@inf.elte.hu>
2023-04-02 12:13:33 +0200

    [CIT-73] > ShaderObject abstraction implemented

---
+++ City Builder/Engine/Abstractions/ShaderObject.h
@@ -0,0 +1,38 @@
+#pragma once
+
+//OpenGL Headers
+#include <GLEW/glew.h>
+
+//Standard Headers
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <initializer_list>
+
+class ShaderObject
+{
+public:
+	ShaderObject(GLuint shaderType, const std::string& filePath);
+	~ShaderObject();
+
+	//Getter
+	inline GLuint Get_ShaderID() { return m_ShaderID; }
+protected:
+	void LoadShader(GLuint shaderType, const std::string& filePath);
+private:
+	GLuint m_ShaderID;
+};
+
+class ShaderObjectLayout
+{
+public:
+	ShaderObjectLayout(GLuint layoutLoc, const std::string& layoutName) : m_LayoutLoc(layoutLoc), m_LayoutName(layoutName) {}
+	~ShaderObjectLayout() {};
+
+	//Getter
+	inline GLuint Get_LayoutLoc() { return m_LayoutLoc; }
+	inline const char* Get_LayoutName() { return m_LayoutName.c_str(); }
+private:
+	GLuint m_LayoutLoc;
+	std::string m_LayoutName;
+};
