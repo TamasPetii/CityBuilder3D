4889efa690035edef055fb644b36d81aa9f50a71
TamasPeti <gr1zh1@inf.elte.hu>
2023-04-02 12:12:00 +0200

    [CIT-70] > VertexArrayObject abstraction implemented

---
+++ City Builder/Engine/Abstractions/VertexArrayObject.h
@@ -0,0 +1,32 @@
+#pragma once
+
+//OpenGL Headers
+#include <GLEW/glew.h>
+#include <GLM/glm.hpp>
+
+//Engine Headers
+#include "Vertex.h"
+#include "BufferObject.h"
+
+//Standard Headers
+#include <vector>
+
+class VertexArrayObject
+{
+public:
+	VertexArrayObject();
+	~VertexArrayObject();
+
+	inline void Bind() const   { glBindVertexArray(m_VertexArrayID); }
+	inline void UnBind() const { glBindVertexArray(0); }
+
+	void LinkAttribute(const BufferObject& buffer, GLuint layout, GLint size, GLenum type, GLsizei stride, const GLvoid* offset) const;
+	void LinkAttributeI(const BufferObject& buffer, GLuint layout, GLint size, GLenum type, GLsizei stride, const GLvoid* offset) const;
+
+	void LinkAttributeInstance(const BufferObject& buffer, GLuint layout, GLint size, GLenum type, GLsizei stride, const GLvoid* offset) const;
+	void LinkAttributeInstanceI(const BufferObject& buffer, GLuint layout, GLint size, GLenum type, GLsizei stride, const GLvoid* offset) const;
+
+private:
+	GLuint m_VertexArrayID;
+};
+
