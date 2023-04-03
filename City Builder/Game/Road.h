f08d752bc5583974cfbd3753562adcda31df075b
Balázs Csató <i0153n@inf.elte.hu>
2023-04-03 12:13:30 +0200

    [CIT-12] > Added initial implementation of Road class

---
+++ City Builder/Game/Road.h
@@ -0,0 +1,15 @@
+#ifndef ROAD_H
+#define ROAD_H
+
+#include "GameField.h"
+
+class Road : public GameField
+{
+public:
+    Road() : GameField() {};
+    ~Road() {}
+
+    bool inline IsRoad() const override { return true; }
+};
+
+#endif
