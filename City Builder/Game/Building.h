b2c3dddfb233da85c014635ca2c5f7e3529ca980
Balázs Csató <i0153n@inf.elte.hu>
2023-04-03 12:15:16 +0200

    [CIT-13] > Added initital implementation of Building class

---
+++ City Builder/Game/Building.h
@@ -0,0 +1,23 @@
+#ifndef BUILDING_H
+#define BUILDING_H
+
+#include "GameField.h"
+
+class Building : public GameField
+{
+protected:
+    Building() : GameField() {};
+
+public:
+    ~Building() {}
+
+    bool inline IsBuilding() const override { return true; }
+    virtual inline bool IsPoliceStation() const { return false; }
+    virtual inline bool IsFireStation() const { return false; }
+    virtual inline bool IsPowerWire() const { return false; }
+    virtual inline bool IsPowerStation() const { return false; }
+    virtual inline bool IsSchool() const { return false; }
+    virtual inline bool IsStadium() const { return false; }
+};
+
+#endif
