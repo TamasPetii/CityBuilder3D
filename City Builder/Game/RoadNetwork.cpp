ad8ee0657e5ead0968fe75738f1db599caa44099
Kiss Ármin Donát <xk2g4r@inf.elte.hu>
2023-04-07 17:51:07 +0200

    [CIT-145] > RoadNetwork class now works with GameField* instead of int coords

--- City Builder/Game/RoadNetwork.cpp
+++ City Builder/Game/RoadNetwork.cpp
@@ -3,11 +3,9 @@
 std::unordered_set<int> RoadNetwork::m_id_set;
 std::vector<RoadNetwork::Network> RoadNetwork::m_networks;
 
-int RoadNetwork::GetNetworkId(int x, int y) {
-	std::pair<int, int> coords = { x, y };
-
+int RoadNetwork::GetNetworkId(GameField* field) {
 	for (auto& network : m_networks) {
-		if (network.fieldSet.find(coords) != network.fieldSet.end())
+		if (network.fieldSet.find(field) != network.fieldSet.end())
 			return network.id;
 	}
 	return -1;
@@ -21,23 +19,21 @@ void RoadNetwork::CreateNetwork() {
 	m_networks.push_back(network);
 }
 
-void RoadNetwork::AddToNetwork(int x, int y, int id) {
-	std::pair<int, int> coords = { x, y };
+void RoadNetwork::AddToNetwork(GameField* field, int id) {
 	for (auto& network : m_networks) {
 		if (network.id != id) continue;
 
-		network.fieldSet.emplace(coords);
+		network.fieldSet.emplace(field);
 	}
 }
 
-void RoadNetwork::RemoveFromNetwork(int x, int y, int id) {
-	std::pair<int, int> coords = { x, y };
+void RoadNetwork::RemoveFromNetwork(GameField* field, int id) {
 	int i = 0;
 	for (auto& network : m_networks) {
 		i++;
 		if (network.id != id) continue;
 
-		network.fieldSet.erase(coords);
+		network.fieldSet.erase(field);
 		if (network.fieldSet.size() == 0) {
 			m_networks.erase(m_networks.begin() + i);
 			m_id_set.erase(network.id);
@@ -45,28 +41,26 @@ void RoadNetwork::RemoveFromNetwork(int x, int y, int id) {
 	}
 }
 
-void RoadNetwork::MergeNetworks(int id1, int id2) {
-	int network1;
-	int network2;
+void RoadNetwork::MergeNetworks(int id1, int id2) { //id: a network id-je. vec_ind: a network indexe a vectorban
+	int vec_ind1 = -1;
+	int vec_ind2 = -1;
 	for (int i = 0; i < m_networks.size(); ++i) {
-		if (id1 == m_networks[i].id) network1 = id1;
-		if (id2 == m_networks[i].id) network2 = id2;
+		if (id1 == m_networks[i].id) vec_ind1 = i;
+		if (id2 == m_networks[i].id) vec_ind2 = i;
 	}
-	RoadNetwork::m_networks[network1].fieldSet.insert( //2. network insertelése az 1.-be
-		std::make_move_iterator(m_networks[network2].fieldSet.begin()), 
-		std::make_move_iterator(m_networks[network2].fieldSet.end())
+	if (vec_ind1 == -1 || vec_ind2 == -1) return;
+	RoadNetwork::m_networks[vec_ind1].fieldSet.insert( //2. network insertelése az 1.-be
+		std::make_move_iterator(m_networks[vec_ind2].fieldSet.begin()), 
+		std::make_move_iterator(m_networks[vec_ind2].fieldSet.end())
 	);
-	m_networks.erase(m_networks.begin() + network2);
-	m_id_set.erase(network2);
+	m_networks.erase(m_networks.begin() + vec_ind2);
+	m_id_set.erase(id2);
 }
 
-bool RoadNetwork::IsConnected(int x1, int y1, int x2, int y2) {
-	std::pair<int, int> coord1 = { x1, y1 };
-	std::pair<int, int> coord2 = { x2, y2 };
-
+bool RoadNetwork::IsConnected(GameField* field1, GameField* field2) {
 	for (auto& network : m_networks) {
-		if (network.fieldSet.find(coord1) == network.fieldSet.end()) continue;
-		if (network.fieldSet.find(coord2) == network.fieldSet.end()) continue;
+		if (network.fieldSet.find(field1) == network.fieldSet.end()) continue;
+		if (network.fieldSet.find(field2) == network.fieldSet.end()) continue;
 		return true;
 	}
 
--- City Builder/Game/RoadNetwork.h
+++ City Builder/Game/RoadNetwork.h
@@ -1,25 +1,25 @@
 #pragma once
 
 #include <vector>
-#include <utility>
 #include <unordered_set>
+#include "GameFields/GameField.h"
 
 class RoadNetwork
 {
 public:
-	static int GetNetworkId(int, int);
+	static int GetNetworkId(GameField*);
 	static void CreateNetwork();
-	static void AddToNetwork(int, int, int);
-	static void RemoveFromNetwork(int, int, int);
+	static void AddToNetwork(GameField*, int);
+	static void RemoveFromNetwork(GameField*, int);
 	static void MergeNetworks(int, int);
-	static bool IsConnected(int, int, int, int);
+	static bool IsConnected(GameField*, GameField*);
 
 private:
 	class Network {
 	public:
 		Network(int i) : id(i) {}
 		const int id;
-		std::unordered_set<std::pair<int, int>> fieldSet;
+		std::unordered_set<GameField*> fieldSet;
 	};
 
 	static std::unordered_set<int> m_id_set; //minden networknek egy különbözõ id-ja lesz
