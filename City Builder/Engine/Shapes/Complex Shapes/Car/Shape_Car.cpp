f5993ab23aa718ef054129ba60acc7e7c29a5685
Balázs Csató <i0153n@inf.elte.hu>
2023-04-21 23:48:11 +0200

    [CIT-220] > Car shape added

---
+++ City Builder/Engine/Shapes/Complex Shapes/Car/Shape_Car.cpp
@@ -0,0 +1,83 @@
+#include "Shape_Car.h"
+
+Shape_Car::Shape_Car()
+{
+	Transform transform;
+	transform.translate = glm::translate(glm::vec3(0, 0.5, 0));
+	transform.rotate = glm::mat4(1);
+	transform.scale = glm::mat4(1);
+
+	shape_transform.push_back(transform);
+}
+
+void Shape_Car::CreateBuffers(GLuint buffer_size)
+{
+	std::vector<Vertex> vertices;
+	std::vector<GLuint> indices;
+
+	CubeLayout cube_layout;
+
+	cube_layout.GeometryDetails.BOTTOM_BACK_LEFT = glm::vec3(-0.25, 0.0, -0.115);
+	cube_layout.GeometryDetails.BOTTOM_BACK_RIGHT = glm::vec3(0.3, 0.0, -0.115);
+	cube_layout.GeometryDetails.BOTTOM_FRONT_LEFT = glm::vec3(-0.25, 0.0, 0.115);
+	cube_layout.GeometryDetails.BOTTOM_FRONT_RIGHT = glm::vec3(0.3, 0.0, 0.115);
+
+	cube_layout.GeometryDetails.TOP_BACK_LEFT = glm::vec3(-0.25, 0.14, -0.115);
+	cube_layout.GeometryDetails.TOP_BACK_RIGHT = glm::vec3(0.3, 0.14, -0.115);
+	cube_layout.GeometryDetails.TOP_FRONT_LEFT = glm::vec3(-0.25, 0.14, 0.115);
+	cube_layout.GeometryDetails.TOP_FRONT_RIGHT = glm::vec3(0.3, 0.14, 0.115);
+
+
+	cube_layout.TextureDetails.FRONT_ID = 67;
+	cube_layout.TextureDetails.BACK_ID = 67;
+	cube_layout.TextureDetails.LEFT_ID = 68;
+	cube_layout.TextureDetails.RIGHT_ID = 66;
+	cube_layout.TextureDetails.TOP_ID = 65;
+	cube_layout.TextureDetails.BOTTOM_ID = 65;
+
+	Shape::ConcatenateIndices(indices, Cube::GenerateIndices(cube_layout, (GLuint)vertices.size()));
+	Shape::ConcatenateVertices(vertices, Cube::GenerateVertices(cube_layout));
+
+	cube_layout.GeometryDetails.BOTTOM_BACK_LEFT = glm::vec3(-0.01, 0.14, -0.115);
+	cube_layout.GeometryDetails.BOTTOM_BACK_RIGHT = glm::vec3(0.16, 0.14, -0.115);
+	cube_layout.GeometryDetails.BOTTOM_FRONT_LEFT = glm::vec3(-0.01, 0.14, 0.115);
+	cube_layout.GeometryDetails.BOTTOM_FRONT_RIGHT = glm::vec3(0.16, 0.14, 0.115);
+
+	cube_layout.GeometryDetails.TOP_BACK_LEFT = glm::vec3(-0.01, 0.21, -0.095);
+	cube_layout.GeometryDetails.TOP_BACK_RIGHT = glm::vec3(0.1, 0.21, -0.095);
+	cube_layout.GeometryDetails.TOP_FRONT_LEFT = glm::vec3(-0.01, 0.21, 0.095);
+	cube_layout.GeometryDetails.TOP_FRONT_RIGHT = glm::vec3(0.1, 0.21, 0.095);
+
+	cube_layout.TextureDetails.FRONT_ID = 64;
+	cube_layout.TextureDetails.BACK_ID = 64;
+	cube_layout.TextureDetails.LEFT_ID = 64;
+	cube_layout.TextureDetails.RIGHT_ID = 64;
+	cube_layout.TextureDetails.TOP_ID = 65;
+	cube_layout.TextureDetails.BOTTOM_ID = 65;
+
+	Shape::ConcatenateIndices(indices, Cube::GenerateIndices(cube_layout, (GLuint)vertices.size()));
+	Shape::ConcatenateVertices(vertices, Cube::GenerateVertices(cube_layout));
+
+	cube_layout.GeometryDetails.BOTTOM_BACK_LEFT = glm::vec3(-0.16, 0.14, -0.115);
+	cube_layout.GeometryDetails.BOTTOM_BACK_RIGHT = glm::vec3(-0.01, 0.14, -0.115);
+	cube_layout.GeometryDetails.BOTTOM_FRONT_LEFT = glm::vec3(-0.16, 0.14, 0.115);
+	cube_layout.GeometryDetails.BOTTOM_FRONT_RIGHT = glm::vec3(-0.01, 0.14, 0.115);
+
+	cube_layout.GeometryDetails.TOP_BACK_LEFT = glm::vec3(-0.1, 0.21, -0.095);
+	cube_layout.GeometryDetails.TOP_BACK_RIGHT = glm::vec3(-0.01, 0.21, -0.095);
+	cube_layout.GeometryDetails.TOP_FRONT_LEFT = glm::vec3(-0.1, 0.21, 0.095);
+	cube_layout.GeometryDetails.TOP_FRONT_RIGHT = glm::vec3(-0.01, 0.21, 0.095);
+
+
+	cube_layout.TextureDetails.FRONT_ID = 64;
+	cube_layout.TextureDetails.BACK_ID = 64;
+	cube_layout.TextureDetails.LEFT_ID = 64;
+	cube_layout.TextureDetails.RIGHT_ID = 64;
+	cube_layout.TextureDetails.TOP_ID = 65;
+	cube_layout.TextureDetails.BOTTOM_ID = 65;
+
+	Shape::ConcatenateIndices(indices, Cube::GenerateIndices(cube_layout, (GLuint)vertices.size()));
+	Shape::ConcatenateVertices(vertices, Cube::GenerateVertices(cube_layout));
+
+	AttachToGPU(vertices, indices, buffer_size);
+}
